590b713e2d93f81956c6c813dc118c1ccd4bc5b5
Seweryn Czykinowski <sevveryn.czykinowski@gmail.com>
2025-05-19 11:53:18 +0200

    [MNT] Run format

--- front/src/routes/__root.tsx
+++ front/src/routes/__root.tsx
@@ -1,5 +1,5 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
 export const Route = createRootRoute({
   component: () => (
@@ -17,4 +17,4 @@ export const Route = createRootRoute({
       <TanStackRouterDevtools />
     </>
   ),
-})
+});
