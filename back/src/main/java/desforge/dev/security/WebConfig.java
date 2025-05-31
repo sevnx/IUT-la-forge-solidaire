54adce3dd8837861ab219d473466a8159c121f88
shrijith <shrijith.santhakumar@etu.u-paris.fr>
2025-05-31 20:49:36 +0200

    wip

--- back/src/main/java/desforge/dev/security/WebConfig.java
+++ back/src/main/java/desforge/dev/security/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
                 .addResourceHandler("/resources/**")
-                .addResourceLocations("file:src/main/resources/")
+                .addResourceLocations("file:src/main/resources/images/")
                 .setCachePeriod(3600)
                 .resourceChain(true)
                 .addResolver(new PathResourceResolver());
