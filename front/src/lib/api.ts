6719c724646a91ee273f75ec300a045479726c90
Seweryn Czykinowski <sevveryn.czykinowski@gmail.com>
2025-06-01 14:26:11 +0200

    feat(api): add HTTPS

--- front/src/lib/api.ts
+++ front/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:2020/api',
+  baseURL: 'https://localhost:2020/api',
   withCredentials: true,
   headers: {
     Accept: 'application/json',
